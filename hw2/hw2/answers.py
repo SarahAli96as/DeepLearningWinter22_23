c9fa13757e27ef2f281f539445705590acb90db2
salisaleh <salisaleh@apple.com>
2022-12-10 11:43:06 +0200

    [DeepLearning][HW2] Commiting what has been done untill the moment

---
+++ hw2/hw2/answers.py
@@ -0,0 +1,407 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 1 (Backprop) answers
+
+part1_q1 = r"""
+**Your answer:**
+
+used references: 
+https://en.wikipedia.org/wiki/Jacobian_matrix_and_determinant - explanation of Jaccobian Calclations.
+
+
+1.A. Given in_features = 1024, out_features = 512, and the layer is fully connected laye
+ --> W shape is = 512 X 1024; also the shape of X is 64x1024
+the shape of the Jacobian is: 64 x 1024 x 64 x 512
+
+1.B. yes, element-ij in the jaccobian J_Yij depends only on row xi as we are deferentiating Y w.r.t on X;
+therfore when calculating the derivations , when i != j -> J_Yji = 0, so for sure teh Jacobian is sparse§
+
+1.C. No, we can calculate the downstream gratdient w.r.t. to the input X using 
+the given  gradient of the output w.r.t. some downstream scalar loss  𝐿 ,  𝛿𝒀=∂𝐿∂𝒀 multiplyin by matrix W.
+
+
+2.A. Shpae of W is 512x1024 and Y is 64x512, thurfore the shape of Jacobian is 64 x 1024 x 64 x 512
+((((A.I Majd )))) please  review this
+2.B. yes, it is sparsed with the same explanantion but in here w.r.t to wi instead of xi.
+
+2.C. No, with the same explanation but n calculation using X instead of W.
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part1_q2 = r"""
+**Your answer:**
+
+A.I Majd - please review the answer
+Reference to be removed: https://www.quora.com/Is-Deep-Learning-possible-without-back-propagation
+
+No, it is not required.
+The back-propagation computes the gradient of the loss function 
+with respect to the weights of the network for a single input–output,as part of the optimization procedure,
+it was proved by researchers that it is so efficient. but it is not required, a different algorithm can be
+used to calculate the gradients for the loss function with respect to the model parameters,
+such as alternate algorithms that implement the chain rule
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+# ==============
+# Part 2 (Optimization) answers
+
+
+def part2_overfit_hp():
+    wstd, lr, reg = 0, 0, 0
+    # TODO: Tweak the hyperparameters until you overfit the small dataset.
+    # ====== YOUR CODE: ======
+    wstd, lr, reg = 0.1, 0.05, 0.01
+    # ========================
+    return dict(wstd=wstd, lr=lr, reg=reg)
+
+
+def part2_optim_hp():
+    wstd, lr_vanilla, lr_momentum, lr_rmsprop, reg, = (
+        0,
+        0,
+        0,
+        0,
+        0,
+    )
+
+    # TODO: Tweak the hyperparameters to get the best results you can.
+    # You may want to use different learning rates for each optimizer.
+    # ====== YOUR CODE: ======
+    wstd, lr_vanilla, lr_momentum, lr_rmsprop, reg, = (
+        0.1, #Following the concept to start with high learning rate and keep dividing by 2
+        0.0125,
+        0.002,
+        0.00025,
+        0.000001,
+    )
+    # ========================
+    return dict(
+        wstd=wstd,
+        lr_vanilla=lr_vanilla,
+        lr_momentum=lr_momentum,
+        lr_rmsprop=lr_rmsprop,
+        reg=reg,
+    )
+
+
+def part2_dropout_hp():
+    wstd, lr, = (
+        0,
+        0,
+    )
+    # TODO: Tweak the hyperparameters to get the model to overfit without
+    # dropout.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(wstd=wstd, lr=lr)
+
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+# ==============
+
+
+# ==============
+# Part 3 (MLP) answers
+
+
+def part3_arch_hp():
+    n_layers = 0  # number of layers (not including output)
+    hidden_dims = 0  # number of output dimensions for each hidden layer
+    activation = "none"  # activation function to apply after each hidden layer
+    out_activation = "none"  # activation function to apply at the output layer
+    # TODO: Tweak the MLP architecture hyperparameters.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(
+        n_layers=n_layers,
+        hidden_dims=hidden_dims,
+        activation=activation,
+        out_activation=out_activation,
+    )
+
+
+def part3_optim_hp():
+    import torch.nn
+    import torch.nn.functional
+
+    loss_fn = None  # One of the torch.nn losses
+    lr, weight_decay, momentum = 0, 0, 0  # Arguments for SGD optimizer
+    # TODO:
+    #  - Tweak the Optimizer hyperparameters.
+    #  - Choose the appropriate loss function for your architecture.
+    #    What you returns needs to be a callable, so either an instance of one of the
+    #    Loss classes in torch.nn or one of the loss functions from torch.nn.functional.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(lr=lr, weight_decay=weight_decay, momentum=momentum, loss_fn=loss_fn)
+
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+part3_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+# Part 4 (CNN) answers
+
+
+def part4_optim_hp():
+    import torch.nn
+    import torch.nn.functional
+
+    loss_fn = None  # One of the torch.nn losses
+    lr, weight_decay, momentum = 0, 0, 0  # Arguments for SGD optimizer
+    # TODO:
+    #  - Tweak the Optimizer hyperparameters.
+    #  - Choose the appropriate loss function for your architecture.
+    #    What you returns needs to be a callable, so either an instance of one of the
+    #    Loss classes in torch.nn or one of the loss functions from torch.nn.functional.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return dict(lr=lr, weight_decay=weight_decay, momentum=momentum, loss_fn=loss_fn)
+
+
+part4_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
+# Part 5 (CNN Experiments) answers
+
+
+part5_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part5_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part5_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part5_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+# ==============
+
+# ==============
+# Part 6 (YOLO) answers
+
+
+part6_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+part6_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+
+part6_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part6_bonus = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
